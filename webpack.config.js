392df814b41591f9967d57982314a964d5e0e942
Sergio_Valbuena <iosergio.valbuena@gmail.com>
2021-06-17 01:09:14 -0500

    CSS added to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -18,15 +19,24 @@ module.exports = {
             use: {
                 loader: 'babel-loader',
             }
+        },
+
+        {
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader,
+            'css-loader'
+            ],
         }
     ]
     },
+   
 
     plugins : [
         new HtmlWebpackPlugin({
             inject: true,
             template: './public/index.html',
             filename: './index.html'
-        })
+        }),
+        new MiniCssExtractPlugin(),
     ]
 }
